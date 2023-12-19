038a287488016cd4869e0b0a7195c76a163700e9
ursgarsan <ursgarsan@alum.us.es>
2023-12-19 20:36:33 +0100

    .

--- proyecto/app/urls.py
+++ proyecto/app/urls.py
@@ -4,6 +4,5 @@ from .views import *
 urlpatterns = [
     path("", home, name='home'),
     path("cargar", cargar, name='cargar'),
-    path("ingresar", ingresar, name='ingresar'),
-    path("loadRS", loadRS, name='loadRS'),
+    path("info", info, name='info'),
 ]
--- proyecto/app/views.py
+++ proyecto/app/views.py
@@ -1,60 +1,22 @@
-from django.shortcuts import render, redirect, get_object_or_404
-
-from .cargar import *
-
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm
-from django.http.response import HttpResponseRedirect
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.contrib import messages
-import shelve
-from .recommendations import  transformPrefs, getRecommendations, topMatches, getRecommendedItems, sim_distance
+from django.db import connection
+from .cargar import cargar_datos
+from .models import Anime, Puntuacion
 
 def home(request):
-
     return render(request, 'index.html')
 
-@login_required(login_url='/ingresar')
 def cargar(request):
-    
-    cargar_datos()
-
-    return render(request, 'index.html')
-
-def ingresar(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        if 'confirmar' in request.POST:
+            cargar_datos()
+            return redirect('info')
 
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'Inicio de sesión exitoso.')
-            return redirect('home')  
-        else:
-            messages.error(request, 'Nombre de usuario o contraseña incorrectos.')
+    return render(request, 'confirmacion.html')
 
-    return render(request, 'ingresar.html')
+def info(request):
+    num_animes = Anime.objects.count()
+    num_puntuaciones = Puntuacion.objects.count()
 
-
-
-def loadDict():
-    Prefs={}  
-    shelf = shelve.open("dataRS")
-    # ratings = Puntuacion.objects.all()
-    ratings = []
-    for ra in ratings:
-        user = ra.id_usuario.id_usuario
-        itemid = ra.id_pelicula.id_pelicula
-        rating = ra.puntuacion
-        Prefs.setdefault(user, {})
-        Prefs[user][itemid] = rating
-    shelf['Prefs']=Prefs
-    shelf['ItemsPrefs']=transformPrefs(Prefs)
-    shelf.close()
-
-def loadRS(request):
-    loadDict()
-    messages.success(request, 'Se ha cargado la matriz')
-    return redirect('home')
+    return render(request, 'info.html', {'num_animes': num_animes, 'num_puntuaciones': num_puntuaciones})
