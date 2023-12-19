1eac17fff3939b2e9922eff6c71c791f5d4ba280
ursgarsan <ursgarsan@alum.us.es>
2023-12-19 19:46:55 +0100

    .

--- proyecto/app/models.py
+++ proyecto/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Anime(models.Model):
     anime_id = models.IntegerField(primary_key=True)
@@ -14,8 +15,7 @@ class Anime(models.Model):
 class Puntuacion(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
 
     def __str__(self):
         return f"{self.user.username} - {self.anime.name} - {self.rating}"
-
