ed6bbd3b1bc0481c38cc9730abaf4f6ed7765589
ursgarsan <ursgarsan@alum.us.es>
2023-12-19 19:45:49 +0100

    .

--- proyecto/app/models.py
+++ proyecto/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Anime(models.Model):
+    anime_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    type = models.CharField(max_length=50)
+    episodes = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Puntuacion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.anime.name} - {self.rating}"
+
